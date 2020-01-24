a362b94833f933a69dd42cf2beaadd82565e3748
daaasbu <danaasbury@gmail.com>
2020-01-23 19:50:28 -0600

    Updates

---
+++ precache-manifest.673c89278bdb41f358596c57bed6f793.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a91eea55c9caea06b7da5937740f7a5",
+    "url": "/lightswitch-client/index.html"
+  },
+  {
+    "revision": "1a25954c91344babb160",
+    "url": "/lightswitch-client/static/css/main.2c6a48fe.chunk.css"
+  },
+  {
+    "revision": "979bbcd2ee8b9d8c4132",
+    "url": "/lightswitch-client/static/js/2.bf2bddaa.chunk.js"
+  },
+  {
+    "revision": "d50198c2b3cb3b9437d9698faa7c978c",
+    "url": "/lightswitch-client/static/js/2.bf2bddaa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1a25954c91344babb160",
+    "url": "/lightswitch-client/static/js/main.e047832a.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/lightswitch-client/static/js/main.e047832a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d841d24fe1509c3606c7",
+    "url": "/lightswitch-client/static/js/runtime-main.67160bd5.js"
+  }
+]);
